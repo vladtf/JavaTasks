97b3344e93c7ae1c2429aeaced93f25d52f5d1b9
vladtf <vendeta0263@gmail.com>
2020-03-23 23:08:56 +0200

    Solved an extra task

---
+++ src/main/java/com/extra_tasks/task_2.java
@@ -0,0 +1,22 @@
+package com.extra_tasks;
+
+import java.util.Scanner;
+
+//Next Dance Move
+//        Time limit: 1000 ms
+//        Memory limit: 256 MB
+//
+//        You've just learned a new dance, SalsaJS. It consists of 44 moves and goes like this:
+//
+//        1, 2, 3, 1, 2, 3, 1, 2, 3, 41,2,3,1,2,3,1,2,3,4
+//
+//        After that, you just keep repeating the sequence above.
+//
+//        What's the NthNth dance move that you need to do, if you're dancing SalsaJS indefinetly?
+public class task_2 {
+    public static void main(String[] args) {
+        int[] results = new int[]{1, 2, 3, 1, 2, 3, 1, 2, 3, 4};
+        int n = new Scanner(System.in).nextInt();
+        System.out.println(results[(n - 1) % results.length]);
+    }
+}
